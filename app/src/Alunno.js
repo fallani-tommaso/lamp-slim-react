c6c1a6cbe9ebc7d2b4b36e0ba97f35c84ff9af2f
lai2-24 <lai2-24@itismeucci.com>
2024-04-29 11:44:59 +0200

    quinto commit

--- app/src/Alunno.js
+++ app/src/Alunno.js
@@ -3,33 +3,77 @@ import {useState} from 'react';
 export default function Alunno({alunno, loadAlunni}){
 
     const [inCancellazione, setInCancellazione] = useState(false);
-    const [inConferma, setInConferma] = useState(false);
+    
+    const [inConfermaCancellazione, setInConfermaCancellazione] = useState(false);
+
+    const [inModifica, setInModifica] = useState(false);
+
+    const [inConfermaModifica, setInConfermaModifica] = useState(false);
+
 
     async function cancellaAlunno(){
         setInCancellazione(true);
-        await fetch(`http://localhost:8080/alunni/${alunno.id}`, {method: "DELETE"});
+        await fetch(`http://localhost:8080/alunni/${alunno.id}`,
+        {
+            method: "DELETE",
+            headers: {'Content-Type': 'application/json'},
+        });
         loadAlunni();
         setInCancellazione(false);
     }
-    function richiediConferma(){
-        setInConferma(true);
+
+    async function modificaAlunno(){
+        setInModifica(true);
+        await fetch(`http://localhost:8080/alunni/${alunno.id}`,
+        {
+            method: "PUT",
+            headers: {'Content-Type': 'application/json'},
+        });
+        loadAlunni();
+        setInModifica(false);
     }
-    function annulla(){
-        setInConferma(false);
+
+    function richiediConfermaCancellazione(){
+        setInConfermaCancellazione(true);
     }
+    function annullaCancellazione(){
+        setInConfermaCancellazione(false);
+    }
+
+    function richiediConfermaModifica(){
+        setInConfermaModifica(true);
+    }
+    function annullaModifica(){
+        setInConfermaModifica(false);
+    }
+
     return(
         <div>
-            {alunno.id} - {alunno.nome} {alunno.cognome} -
+            {alunno.id} - {alunno.nome} {alunno.cognome}
             { inCancellazione ?
                 <span> in cancellazione... </span>
             :
                 <span>
-                    { inConferma ?
+                    { inConfermaCancellazione ?
                         <span>Sei sicuro? 
                         <button onClick={cancellaAlunno}>si</button>
-                        <button onClick={annulla}>no</button></span>
+                        <button onClick={annullaCancellazione}>no</button></span>
                         :
-                        <button onClick={richiediConferma}>Cancella alunno</button>
+                        <button onClick={richiediConfermaCancellazione}>Cancella</button>
+                    }
+                </span>
+            }
+            
+            { inModifica ?
+                <span> Aggiornamento in corso... </span>
+            :
+                <span>
+                    { inConfermaModifica ?
+                        <span>Sei sicuro?
+                        <button onClick={modificaAlunno}>si</button>
+                        <button onClick={annullaModifica}>no</button></span>
+                        :
+                        <button onClick={richiediConfermaModifica}>Modifica</button>
                     }
                 </span>
             }
